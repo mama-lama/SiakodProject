2aa084af8cd0f1206d66de7256767615af828c6f
MoonBunny70 <dsidelnikov671@gmail.com>
2025-09-24 14:07:51 +0300

    Adding 'blanks' for the ArrayList and PriorityQueue classes in a single package. Minor README update

---
+++ src/ru/vsu/sc/siakod25/g11/theme_6/demo_app/Main.java
@@ -0,0 +1,7 @@
+package ru.vsu.sc.siakod25.g11.theme_6.demo_app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/ru/vsu/sc/siakod25/g11/theme_6/lor_app/PriorityQueue.java
@@ -0,0 +1,17 @@
+package ru.vsu.sc.siakod25.g11.theme_6.lor_app;
+
+public class PriorityQueue {
+    private int size;
+
+    public PriorityQueue(){}
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+}
